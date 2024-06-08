c639c52bf84599b6a4c9f66100e704f4af0a59d6
DKK2311 <164287208+DKK2311@users.noreply.github.com>
2024-06-08 18:31:03 +0530

    spicejet project

---
+++ src/main/java/pages/Addonspage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import base.ProjectSpecificationMethod;
+
+public class Addonspage  extends ProjectSpecificationMethod {
+	
+	public Addonspage  (WebDriver sdriver) {
+		this.driver=driver;
+	}
+	
+	//To click continue button to add on page
+	public Addonspage continuebutton() {
+	return this;
+	
+}
+	}
